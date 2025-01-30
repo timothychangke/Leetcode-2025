e1211592ee3e662507e209a0c4831a7df69cf57f
Tim chang <changkaien0101@gmail.com>
2025-01-30 23:15:20 +0800

    completed first try 300125

---
+++ Sliding-Window/1.M-Permutation in String.py
@@ -0,0 +1,18 @@
+from collections import Counter
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        c, l, m = Counter(s1), len(s1), 0
+        for i in range(len(s2)):
+            if s2[i] in c:
+                if not c[s2[i]]: m -= 1
+                c[s2[i]] -= 1 
+                if not c[s2[i]]: m += 1
+            if i >= l and s2[i - l] in c:
+                if not c[s2[i - l]]: m -= 1
+                c[s2[i - l]] += 1
+                if not c[s2[i - l]]: m += 1
+            if m == len(c):
+                return True
+            print(c)
+        return False
+                
