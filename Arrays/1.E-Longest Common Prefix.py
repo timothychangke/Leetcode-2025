17488fe0b5043570fcd354a54899a9cf3fa52d0a
Tim chang <changkaien0101@gmail.com>
2025-02-14 09:13:31 +0800

    fix(Arrays/1.E-Longest Common Prefix.py): file rename

--- Arrays/1.E-Longest Common Prefix.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def longestCommonPrefix(self, strs: List[str]) -> str:
-        res = ''
-        for i in range(min(len(str) for str in strs)):
-            for s in strs:
-                if s[i] != strs[0][i]:
-                    return res
-            res += s[i]
-        return res
