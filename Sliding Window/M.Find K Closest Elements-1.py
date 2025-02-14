69321899818d716c826b3a8435152af9407c3280
Tim chang <changkaien0101@gmail.com>
2025-02-14 09:13:29 +0800

    fix(Sliding Window/M.Find K Closest Elements-1.py): file rename

---
+++ Sliding Window/M.Find K Closest Elements-1.py
@@ -0,0 +1,16 @@
+class Solution:
+    def findClosestElements(self, arr: List[int], k: int, x: int) -> List[int]:
+        def closer(a, b, x):
+            if abs(a - x) < abs(b - x) or (abs(a - x) == abs(b - x) and a < b): return True
+            else: return False
+                
+        l = 0
+        while l + k < len(arr):
+            print(l)
+            if closer(arr[l], arr[l + k], x):
+                break
+            else:
+                l += 1
+        return arr[l:l + k]
+                
+                
