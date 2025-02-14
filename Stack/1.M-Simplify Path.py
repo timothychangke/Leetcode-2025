376c823a2d7c070073318b99e3ca254c1ec57c22
Tim chang <changkaien0101@gmail.com>
2025-02-14 09:13:34 +0800

    fix(Stack/1.M-Simplify Path.py): file rename

--- Stack/1.M-Simplify Path.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def simplifyPath(self, path: str) -> str:
-        stack = []
-        for ele in path.split("/"):
-            if stack and ele == '..':
-                stack.pop()
-            elif ele not in ['', '.', '..']:
-                stack.append(ele)
-        return "/" + "/".join(stack)
