88eb3cc28cc0df4fea7fbff1737bb160d30edb25
Tim chang <changkaien0101@gmail.com>
2025-02-14 09:13:31 +0800

    fix(Arrays/1.E-Two Sum.py): file rename

--- Arrays/1.E-Two Sum.py
+++
@@ -1,7 +0,0 @@
-class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        map = {}
-        for idx, num in enumerate(nums):
-            if target - num in map:
-                return (idx, map[target - num])
-            map[num] = idx
