519edf65f14df2e6598032f899c85315392e9354
Tim chang <changkaien0101@gmail.com>
2025-02-15 10:17:01 +0800

    feat(Arrays/E.Group Anagrams-3.py): third completed 2025-02-15

--- Arrays/E.Group Anagrams-3.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        d = collections.defaultdict(list)
-        for str in strs:
-            arr = [0] * 26
-            for let in str: arr[ord(let) - ord('a')] +=1
-            d[tuple(arr)].append(str)
-        return list(d.values())
