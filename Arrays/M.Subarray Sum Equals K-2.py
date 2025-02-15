fb640e288c127ce5f7f6db4f454e0bce5429e074
Tim chang <changkaien0101@gmail.com>
2025-02-16 01:56:44 +0800

    feat(Arrays/M.Subarray Sum Equals K-2.py): second completed 2025-02-16

---
+++ Arrays/M.Subarray Sum Equals K-2.py
@@ -0,0 +1,17 @@
+class Solution:
+    def subarraySum(self, nums: List[int], k: int) -> int:
+        prefix = 0
+        map = {0: 1}
+        res = 0
+        for n in nums:
+            prefix += n
+            diff = prefix - k
+            res += map.get(diff, 0)
+            map[prefix] = map.get(prefix, 0) + 1
+        return res
+    
+""" 
+1 1 1
+p = 0
+dict = {0: 1, 1: 1}
+"""
