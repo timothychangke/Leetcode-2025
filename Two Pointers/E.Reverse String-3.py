63f69e8ca864d00070558e83480d468a9e056fe5
Tim chang <changkaien0101@gmail.com>
2025-02-16 16:42:50 +0800

    feat(Two Pointers/E.Reverse String-3.py): third completed 2025-02-16

---
+++ Two Pointers/E.Reverse String-3.py
@@ -0,0 +1,21 @@
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
+        """
+        Do not return anything, modify s in-place instead.
+        """
+        l, r = 0, len(s) - 1
+        while l < r:
+            s[l], s[r] = s[r], s[l]
+            l += 1
+            r -= 1
+""" 
+h e l l o
+o l l e h
+r 2
+l 2
+
+hellos
+solleh
+l 2
+r 3
+"""
