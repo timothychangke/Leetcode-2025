a69903c07aa232ba48f30ba712aaa3f0fb0c3ba7
Tim chang <changkaien0101@gmail.com>
2025-02-16 16:42:50 +0800

    feat(Two Pointers/E.Merge Sorted Array-3.py): third completed 2025-02-16

---
+++ Two Pointers/E.Merge Sorted Array-3.py
@@ -0,0 +1,25 @@
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        """
+        Do not return anything, modify nums1 in-place instead.
+        """
+        i, j, k = m - 1, n - 1, len(nums1) - 1
+        while j >= 0:
+            if i >= 0 and nums1[i] > nums2[j]:
+                nums1[k] = nums1[i]
+                i -= 1
+            else:
+                nums1[k] = nums2[j]
+                j -= 1
+            k -= 1
+            
+""" 
+nums1: 7 8 9 0 0 0 
+nums2: 2 5 6
+res 7 8 9 7 8 9 
+i 2
+j 0
+k 2
+"""
+
+        
