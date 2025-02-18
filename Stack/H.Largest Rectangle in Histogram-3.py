8999469d24590ab10f44915d17dd5a522127dbac
tim <changkaien0101@gmail.com>
2025-02-18 16:11:51 +0800

    feat(Stack/H.Largest Rectangle in Histogram-3.py): third attempt completed 2025-02-18

--- Stack/H.Largest Rectangle in Histogram-3.py
+++ Stack/H.Largest Rectangle in Histogram-3.py
@@ -1,11 +1,11 @@
 class Solution:
     def largestRectangleArea(self, heights: List[int]) -> int:
         heights.append(0)
-        stack = []
+        stack = [-1]
         res = 0
         for i in range(len(heights)):
-            while heights[i] < stack[-1]:
+            while heights[i] < heights[stack[-1]]:
                 h = stack.pop()
-                res = max(res, (i - h) * heights[h])
+                res = max(res, (i - stack[-1] - 1) * heights[h])
             stack.append(i)
         return res
