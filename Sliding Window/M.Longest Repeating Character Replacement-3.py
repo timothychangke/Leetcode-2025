b1d1499bdcc2ed46921f50229e0d928f2fdba5eb
tim <changkaien0101@gmail.com>
2025-02-21 00:16:14 +0800

    feat(Sliding Window/M.Longest Repeating Character Replacement-3.py): third attempt completed 2025-02-21

--- Sliding Window/M.Longest Repeating Character Replacement-3.py
+++ Sliding Window/M.Longest Repeating Character Replacement-3.py
@@ -1,3 +1,20 @@
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
-        
+        arr = [0] * 26
+        maxC, l = 0 , 0
+        for r in range(len(s)):
+            arr[ord(s[r]) - ord('A')] += 1
+            maxC = max(arr[ord(s[r]) - ord('A')], maxC)
+            if r - l + 1 - maxC > k:
+                arr[ord(s[l]) - ord('A')] -= 1
+                l += 1
+        return len(s) - l 
+
+
+""" 
+s = "AABABBA", k = 1
+Output: 4
+window size - k = max frequency
+
+ """
+            
