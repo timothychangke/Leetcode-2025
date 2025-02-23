f53bb4993b4028d6abc81924645cea0ba19bfe1b
tim <changkaien0101@gmail.com>
2025-02-23 22:43:38 +0800

    feat(Binary Search/E.First Bad Version-3.py): third attempt completed 2025-02-23

---
+++ Binary Search/E.First Bad Version-3.py
@@ -0,0 +1,15 @@
+# The isBadVersion API is already defined for you.
+# def isBadVersion(version: int) -> bool:
+
+class Solution:
+    def firstBadVersion(self, n: int) -> int:
+        l, r = 0, n
+        res = n + 1
+        while l <= r:
+            mid = l + (r - l) // 2
+            if isBadVersion(mid):
+                res = mid
+                r = mid - 1
+            else:
+                l = mid + 1
+        return res
