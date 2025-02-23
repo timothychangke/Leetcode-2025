2d38b68f7b1fce86923b0b691522d7328d0b1b31
tim <changkaien0101@gmail.com>
2025-02-24 07:17:53 +0800

    feat(Linked List/M.LRU Cache-1.py): first attempt completed 2025-02-24

---
+++ Linked List/M.LRU Cache-1.py
@@ -0,0 +1,25 @@
+class ListNode:
+    def __init__(self, val, left, right):
+        self.val, self.left, self.right = val, left, right
+
+class LRUCache:
+
+    def __init__(self, capacity: int):
+        self.leftNode = (0, None, None)
+        self.rightNode = (0, self.leftNode, None)
+        self.leftNode.right = self.rightNode
+        self.cache = {}
+        self.capacity = capacity
+
+    def get(self, key: int) -> int:
+        
+        
+
+    def put(self, key: int, value: int) -> None:
+        
+
+
+# Your LRUCache object will be instantiated and called as such:
+# obj = LRUCache(capacity)
+# param_1 = obj.get(key)
+# obj.put(key,value)
