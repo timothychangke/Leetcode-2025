f8b0d6a081994ec53618b00212007f540bbb5334
tim <changkaien0101@gmail.com>
2025-02-25 21:47:23 +0800

    feat(Binary Search/M.Minimum Number of Days to make M Bouquets-2.py): second attempt completed 2025-02-25

--- Binary Search/M.Minimum Number of Days to make M Bouquets-2.py
+++ Binary Search/M.Minimum Number of Days to make M Bouquets-2.py
@@ -8,16 +8,17 @@ class Solution:
                 if b <= x:
                     c += 1
                 else:
-                    if c >= k: ans += 1
+                    ans += c // k
                     c = 0
-                return ans >= m
+            ans += c // k
+            return ans >= m
         res = -1
         l, r = min(bloomDay), max(bloomDay)
         while l <= r:
             mid = l + (r - l) // 2
             if feasible(mid):
                 res = mid
-                r = mid
+                r = mid - 1
             else:
                 l = mid + 1
         return res
