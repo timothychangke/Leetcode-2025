63bfd1c593632b1a1a71138ab3bc59d51322aea6
tim <changkaien0101@gmail.com>
2025-03-02 03:19:09 +0800

    feat(Trees/M.Insert into Binary Search Tree-2.py): second attempt completed 2025-03-02

---
+++ Trees/M.Insert into Binary Search Tree-2.py
@@ -0,0 +1,16 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def insertIntoBST(self, root: Optional[TreeNode], val: int) -> Optional[TreeNode]:
+        def dfs(root):
+            if not root: return TreeNode(val)
+            if root.val > val:
+                root.left = dfs(root.left)
+            elif root.val < val:
+                root.right = dfs(root.right)
+            return root
+        return dfs(root)
