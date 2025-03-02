2aae1ad20ec48e9db463d9b65cdb73477e45ea49
tim <changkaien0101@gmail.com>
2025-03-02 23:42:55 +0800

    feat(Linked List/H.Merge K Sorted Lists-2.py): second attempt completed 2025-03-02

--- Linked List/H.Merge K Sorted Lists-2.py
+++ Linked List/H.Merge K Sorted Lists-2.py
@@ -5,4 +5,22 @@
 #         self.next = next
 class Solution:
     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+        if not lists: return None
+        if len(lists) == 1: return lists[0]
+        mid = len(lists) // 2
+        l, r = self.mergeKLists(lists[:mid]), self.mergeKLists(lists[mid:])
+        return self.merge(l, r)
+    def merge(self, l, r):
+        head = cur = ListNode()
+        while l and r:
+            if l.val < r.val:
+                cur.next = l
+                l = l.next
+            else:
+                cur.next = r
+                r = r.next
+            cur = cur.next
+        if l: cur.next = l
+        if r: cur.next = r
+        return head.next
         
