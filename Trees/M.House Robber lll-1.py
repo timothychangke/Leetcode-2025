a64b9bca918e6f7fd37d338ffe2991dd1dd175a1
tim <changkaien0101@gmail.com>
2025-03-06 23:53:46 +0800

    feat(Trees/M.House Robber lll-1.py): first attempt completed 2025-03-06

---
+++ Trees/M.House Robber lll-1.py
@@ -0,0 +1,17 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def rob(self, root: Optional[TreeNode]) -> int:
+        def dfs(node):
+            if not node: return [0,0]
+            lwith, lwithout = dfs(node.left)
+            rwith, rwithout = dfs(node.right)
+            return [node.val + lwithout + rwithout, max(lwith, lwithout) + max(rwith, rwithout)]
+        w, wo = dfs(root)
+        return max(w, wo)
+
+        
