c6e94496e64161c793c36eecd237f9acebe1eb39
tim <changkaien0101@gmail.com>
2025-03-09 23:22:59 +0800

    feat(Heap/M.Reorganize String-1.py): first attempt completed 2025-03-09

---
+++ Heap/M.Reorganize String-1.py
@@ -0,0 +1,29 @@
+import heapq
+from collections import Counter
+
+
+class Solution:
+    def reorganizeString(self, s: str) -> str:
+        ctr = Counter(s)
+        max_f = max(ctr.values())
+        if max_f > len(s) // 2:
+            return ""
+        heap = [[-c, let] for let, c in ctr.items()]
+        heapq.heapify(heap)
+        res = heapq.heappop(heap)[1]
+        prev = res
+        while heap:
+            count, let = heapq.heappop(heap)
+            if let == prev:
+                count2, let2 = heapq.heappop(heap)
+                res += let2
+                count2 += 1
+                if count2 < 0:
+                    heapq.heappush([count2, let2])
+            else:
+                res += let
+                count += 1
+            if count < 0:
+                heapq.heappush([count, let])
+        return res
+                
