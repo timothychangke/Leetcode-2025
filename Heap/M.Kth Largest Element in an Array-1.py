d80670a173413869cc05fcec919d69d4141fb5f3
tim <changkaien0101@gmail.com>
2025-03-09 20:36:50 +0800

    feat(Heap/M.Kth Largest Element in an Array-1.py): first attempt completed 2025-03-09

---
+++ Heap/M.Kth Largest Element in an Array-1.py
@@ -0,0 +1,10 @@
+import heapq
+class Solution:
+    def findKthLargest(self, nums: List[int], k: int) -> int:
+        heap = nums[:k]
+        heapq.heapify(heap)
+        for i in range(k, len(nums)):
+            if nums[i] > heap[0]:
+                heapq.heappushpop(heap, nums[i])
+        return heap[0]
+        
