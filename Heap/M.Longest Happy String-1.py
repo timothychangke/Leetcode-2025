4520c1a377ae8b850c15fd5e061da1fdfa35187a
tim <changkaien0101@gmail.com>
2025-03-10 00:47:18 +0800

    feat(Heap/M.Longest Happy String-1.py): first attempt completed 2025-03-10

--- Heap/M.Longest Happy String-1.py
+++ Heap/M.Longest Happy String-1.py
@@ -4,16 +4,23 @@ import heapq
 class Solution:
     def longestDiverseString(self, a: int, b: int, c: int) -> str:
         res = ''
-        prev = ''
-        heap = [-a, 'a'], [-b, 'b'], [-c, 'c']
-        heapq.heapify(heap)
+        heap = []
+        for c, let, in [[a, 'a'], [b, 'b'], [c, 'c']]:
+            if c > 0:
+                heapq.heappush(heap, [-c, let])
         while heap:
             c, let = heapq.heappop(heap)
-            if let == prev: return ""
-            res.append(let + let if c > 1 else let)
-            prev = let
-            c += 1
-            if c > 0:
-                heapq.heappush([c, let])
+            if len(res) >=2 and res[-1] == res[-2] == let:
+                if not heap: break
+                c2, let2 = heapq.heappop(heap)
+                res += let2
+                c2 += 1
+                if c2 < 0: heapq.heappush(heap, [c2, let2])
+            else:
+                res += let
+                c += 1
+                if c >= 0: continue
+            heapq.heappush(heap, [c, let])
         return res
+        
             
