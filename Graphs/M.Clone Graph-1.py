3dd521fba9b3a5dab77a1bb921acd42b0cf00c8d
tim <changkaien0101@gmail.com>
2025-03-13 22:59:33 +0800

    feat(Graphs/M.Clone Graph-1.py): first attempt completed 2025-03-13

--- Graphs/M.Clone Graph-1.py
+++ Graphs/M.Clone Graph-1.py
@@ -9,4 +9,13 @@ class Node:
 from typing import Optional
 class Solution:
     def cloneGraph(self, node: Optional['Node']) -> Optional['Node']:
+        if not node: return None
+        oldToNew = {}
+        def dfs(node):
+            if node in oldToNew: return oldToNew[node]
+            oldToNew[node] = Node(node.val)
+            for nei in node.neighbors:
+                oldToNew[node].neighbors.append(dfs(nei))
+            return oldToNew[node]
+        return dfs(node)
         
