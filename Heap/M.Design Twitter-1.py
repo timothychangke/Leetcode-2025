be70d0973ca3c34ae3d43a070d8cfdde5cb4ccad
tim <changkaien0101@gmail.com>
2025-03-15 21:44:38 +0800

    feat(Heap/M.Design Twitter-1.py): first attempt completed 2025-03-15

--- Heap/M.Design Twitter-1.py
+++ Heap/M.Design Twitter-1.py
@@ -5,23 +5,24 @@ class Twitter:
     def __init__(self):
         self.time = 0
         self.followList = defaultdict(set)
-        self.tweets = defaultdict(list)
+        self.tweetList = defaultdict(list)
+        
 
     def postTweet(self, userId: int, tweetId: int) -> None:
-        self.tweets[userId].append([self.time, tweetId])
+        self.tweetList[userId].append([self.time, tweetId])
         self.time -= 1
 
     def getNewsFeed(self, userId: int) -> List[int]:
-        self.followList[userId].add(userId)
-        user_feed = [tweet for follower in self.followList[userId] for tweet in self.tweets[follower]]
-        return [tweetId for _, tweetId in heapq.nsmallest(10, user_feed)]
+        if userId not in self.followList[userId]: self.followList[userId].add(userId)
+        tweets = [tweet for id in self.followList[userId] for tweet in self.tweetList[id] ]
+        return [tweetId for _, tweetId in heapq.nsmallest(10, tweets)]
         
+
     def follow(self, followerId: int, followeeId: int) -> None:
-        self.followList[followerId].add(followeeId)
+       self.followList[followerId].add(followeeId) 
 
     def unfollow(self, followerId: int, followeeId: int) -> None:
-        if followeeId in self.followList[followerId]:
-            self.followList[followerId].remove(followeeId)
+        if followeeId in self.followList[followerId]: self.followList[followerId].remove(followeeId)
         
 
 
@@ -31,10 +32,3 @@ class Twitter:
 # param_2 = obj.getNewsFeed(userId)
 # obj.follow(followerId,followeeId)
 # obj.unfollow(followerId,followeeId)
-
-""" 
-with a hash set, items can be added and removed in O(1), this will work for the follow and unfollow functions
-need to map a userid to the list of their tweets, can be achieved in O(1)
-to get news feed, you merge k sorted lists
-
-"""
