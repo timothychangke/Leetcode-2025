90a1dd9598a840b79893bbfccc718a191dcde6df
tim <changkaien0101@gmail.com>
2025-03-15 23:57:29 +0800

    feat(Heap/H.Find Median from Data Stream-2.py): second attempt completed 2025-03-15

--- Heap/H.Find Median from Data Stream-2.py
+++ Heap/H.Find Median from Data Stream-2.py
@@ -1,13 +1,19 @@
+import heapq
 class MedianFinder:
 
     def __init__(self):
-        
+        self.maxHeap = []
+        self.minHeap = []
 
     def addNum(self, num: int) -> None:
+        heapq.heappush(self.maxHeap, -heapq.heappushpop(self.minHeap, num))
+        if len(self.maxHeap) > len(self.minHeap): heapq.heappush(self.minHeap, -heapq.heappop(self.maxHeap))
         
 
     def findMedian(self) -> float:
-        
+        if (len(self.minHeap) + len(self.maxHeap)) % 2: return self.minHeap[0]
+        else: return (-self.maxHeap[0] + self.minHeap[0]) / 2
+            
 
 
 # Your MedianFinder object will be instantiated and called as such:
