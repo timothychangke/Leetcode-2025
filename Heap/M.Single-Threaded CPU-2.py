5489d6e8c75f71e920fa279e3ab31328805fd88b
tim <changkaien0101@gmail.com>
2025-03-15 23:31:49 +0800

    feat(Heap/M.Single-Threaded CPU-2.py): second attempt completed 2025-03-15

--- Heap/M.Single-Threaded CPU-2.py
+++ Heap/M.Single-Threaded CPU-2.py
@@ -1,3 +1,15 @@
+import heapq
 class Solution:
     def getOrder(self, tasks: List[List[int]]) -> List[int]:
-        
+        res = []
+        time = 0
+        i = 0
+        h = []
+        while i < len(tasks):
+            while tasks[i][0] < time:
+                heapq.heappush(h, (tasks[i][1], i))
+                i += 1
+            processingTime, i = heapq.heappop(h)
+            time += processingTime
+            res.append(i)
+        return res 
