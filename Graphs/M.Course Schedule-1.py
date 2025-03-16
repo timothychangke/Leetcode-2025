1cc3d6c5495ff5fd02132696ebd11f81123ef491
tim <changkaien0101@gmail.com>
2025-03-16 12:15:21 +0800

    feat(Graphs/M.Course Schedule-1.py): first attempt completed 2025-03-16

---
+++ Graphs/M.Course Schedule-1.py
@@ -0,0 +1,20 @@
+from collections import deque
+class Solution:
+    def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+        g = [[] for _ in range(numCourses)]
+        degree = [0] * numCourses
+        visited = 0
+        res = []
+        for a, b in prerequisites:
+            g[b].append(a)
+            degree[a] += 1
+        q = deque([i for i, d in enumerate(degree) if not d])
+        while q:
+            cur = q.popleft()
+            visited += 1
+            res.append(cur)
+            for nei in g[cur]:
+                degree[nei] -= 1
+                if not degree[nei]:
+                    q.append(nei)
+        return res if visited == numCourses else []
