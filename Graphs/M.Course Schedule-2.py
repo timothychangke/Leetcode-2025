dbedd0260a9a9fd2a7935800faa693c90d62746d
tim <changkaien0101@gmail.com>
2025-03-16 12:15:21 +0800

    feat(Graphs/M.Course Schedule-2.py): second attempt completed 2025-03-16

---
+++ Graphs/M.Course Schedule-2.py
@@ -0,0 +1,22 @@
+from collections import deque
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        g = [[] for i in range(numCourses)]
+        degree = [0] * numCourses
+        for a, b in prerequisites:
+            g[b].append(a)
+            degree[a] += 1
+        q = deque([i for i, d in enumerate(degree) if not d])
+        c = 0
+        while q:
+            for _ in range(len(q)):
+                course = q.popleft()
+                c += 1
+                for nei in g[course]:
+                    degree[nei] -= 1
+                    if not degree[nei]:
+                        q.append(nei)
+        return c == numCourses
+
+
+        
