d9530db719f1a818dc76e7c7867dcfc274590fb4
tim <changkaien0101@gmail.com>
2025-03-17 22:28:11 +0800

    feat(Company Question/ByteDance/M.Remove Duplicate Letters-1.py): first attempt completed 2025-03-17

---
+++ Company Question/ByteDance/M.Remove Duplicate Letters-1.py
@@ -0,0 +1,14 @@
+class Solution:
+    def removeDuplicateLetters(self, s: str) -> str:
+        d = {}
+        for i, c in enumerate(s):
+            d[c] = i
+        seen = set()
+        stack = []
+        for i, c in enumerate(s):
+            if c in seen: continue
+            while stack and stack[-1] > c and d[stack[-1]] > i:
+                seen.remove(stack.pop())
+            stack.append(c)
+            seen.add(c)
+        return ''.join(stack) 
