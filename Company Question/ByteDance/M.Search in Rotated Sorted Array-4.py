e26213931d25f0b6d2ee2b81210d5e53730bf8b8
tim <changkaien0101@gmail.com>
2025-03-19 22:28:25 +0800

    feat(Company Question/ByteDance/M.Search in Rotated Sorted Array-4.py): fourth attempt completed 2025-03-19

---
+++ Company Question/ByteDance/M.Search in Rotated Sorted Array-4.py
@@ -0,0 +1,17 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        l, r = 0, len(nums) - 1
+        while l <= r:
+            mid = l + (r - l) // 2
+            if target == nums[mid]: return mid 
+            elif nums[mid] >= nums[l]:
+                if target > nums[mid] or target < nums[l]:
+                    l = mid + 1
+                else:
+                    r = mid - 1
+            else:
+                if target < nums[mid] or target >= nums[l]:
+                    r = mid - 1
+                else:
+                    l = mid + 1 
+        return -1
