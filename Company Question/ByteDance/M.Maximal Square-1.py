a169a4ed03ed2d9a73739ba567a64018553ce67b
tim <changkaien0101@gmail.com>
2025-03-20 00:08:37 +0800

    feat(Company Question/ByteDance/M.Maximal Square-1.py): first attempt completed 2025-03-20

--- Company Question/ByteDance/M.Maximal Square-1.py
+++ Company Question/ByteDance/M.Maximal Square-1.py
@@ -3,10 +3,13 @@ class Solution:
         rows, cols = len(matrix), len(matrix[0])
         cache = {}
         def dfs(r, c):
-            if r in range(rows) and c in range(cols):
-                if (r, c) in cache: return cache[(r, c)]
+            if r not in range(rows) or c not in range(cols): return 0
+            if (r, c) not in cache: 
                 right = dfs(r, c + 1)
                 bottom = dfs(r + 1, c)
                 diagonal = dfs(r + 1, c + 1)
                 cache[(r, c)] = 1 + min(right, bottom, diagonal) if matrix[r][c] == '1' else 0
-        return max(cache.values())
+            return cache[(r, c)]
+        dfs(0, 0)
+        s = max(cache.values())
+        return s * s
