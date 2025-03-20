3d5c6ece411f5723a4dce770229b37cca162b98e
tim <changkaien0101@gmail.com>
2025-03-20 23:58:45 +0800

    feat(Company Question/ByteDance/M.Insert Delete GetRandom O(1)-1.py): first attempt completed 2025-03-20

---
+++ Company Question/ByteDance/M.Insert Delete GetRandom O(1)-1.py
@@ -0,0 +1,42 @@
+import random
+class RandomizedSet:
+
+    def __init__(self):
+        self.a, self.d = [], {}
+        
+
+    def insert(self, val: int) -> bool:
+        if val not in self.d:
+            self.a.append(val)
+            self.d[val] = len(self.a) - 1
+            return True
+        return False
+
+    def remove(self, val: int) -> bool:
+        if val in self.d:
+            pos = self.d[val]
+            val_of_last = self.a[-1]
+            self.d[val_of_last] = pos
+            self.a[pos] = pos
+            self.a.pop()
+            del self.d[val]
+            return True
+        return False
+        
+
+    def getRandom(self) -> int:
+        return self.a[random.randint(0, len(self.a) - 1)]
+    
+    """ 
+    ["RandomizedSet","insert","insert","remove","insert","remove","getRandom"]
+[[],[0],[1],[0],[2],[1],[]]
+array = 
+dict
+    """
+
+
+# Your RandomizedSet object will be instantiated and called as such:
+# obj = RandomizedSet()
+# param_1 = obj.insert(val)
+# param_2 = obj.remove(val)
+# param_3 = obj.getRandom()
