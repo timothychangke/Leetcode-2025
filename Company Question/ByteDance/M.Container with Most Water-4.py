7e8ccef96ea409b5171fcdde7f5f04f4c16f892b
tim <changkaien0101@gmail.com>
2025-03-21 23:40:07 +0800

    feat(Company Question/ByteDance/M.Container with Most Water-4.py): fourth attempt completed 2025-03-21

---
+++ Company Question/ByteDance/M.Container with Most Water-4.py
@@ -0,0 +1,12 @@
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        l, r = 0, len(height) - 1
+        res = 0
+        while l < r:
+            if min(height[l], height[r]) * (r - l):
+                res = max(min(height[l], height[r]) * (r - l), res)
+            if height[l] > height[r]:
+                r -= 1
+            else:
+                l += 1
+        return res
