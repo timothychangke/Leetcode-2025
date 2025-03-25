d8f191a35137985ac0431d3c5c03a29c3bb3547f
tim <changkaien0101@gmail.com>
2025-03-25 21:53:09 +0800

    feat(Stack/M.Asteriod Collision-3.py): third attempt completed 2025-03-25

--- Stack/M.Asteriod Collision-3.py
+++ Stack/M.Asteriod Collision-3.py
@@ -1,21 +1,5 @@
 class Solution:
     def asteroidCollision(self, asteroids: List[int]) -> List[int]:
-        # stack = []
-        # for a in asteroids:
-        #     if stack and a < 0:
-        #         while stack and stack[-1] > 0 and a < 0:
-        #             if abs(a) > abs(stack[-1]):
-        #                 stack.pop()
-        #             elif abs(a) == abs(stack[-1]):
-        #                 stack.pop()
-        #                 break
-        #             else:
-        #                 break
-        #         else:
-        #             stack.append(a)
-        #     else:
-        #         stack.append(a)
-        # return stack
         stack = []
         for a in asteroids:
             while stack and stack[-1] > 0 and a < 0:
