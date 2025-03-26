da6fdf286163965d6c741d0c54dc78646cda71bc
tim <changkaien0101@gmail.com>
2025-03-26 23:19:29 +0800

    feat(1-D Dynamic Programming/E.Climbing Stairs-2.py): second attempt completed 2025-03-26

---
+++ 1-D Dynamic Programming/E.Climbing Stairs-2.py
@@ -0,0 +1,20 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+       if n < 2: return n
+       prev, prev2 = 1, 1
+       res = 0
+       for _ in range(2, n + 1):
+           res = prev + prev2
+           prev2, prev = prev, res
+       return res
+
+ """ 
+ Ideation:
+ Distinct ways to reach n when at each time you can climb 1 or 2 steps
+ the distinct way to reach dp[i] = dp[i - 1] + dp[i - 2]
+ 
+ for each value of i:
+ dp[i] = dp[i - 1] + dp[i - 2]
+ to save space, can use variables to represent the different values instead of an array
+ 
+ """
