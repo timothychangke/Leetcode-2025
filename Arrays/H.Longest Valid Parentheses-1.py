6751dd0a2144d9f1b833e977c4a7ebfe85787389
tim <changkaien0101@gmail.com>
2025-04-06 22:57:17 +0800

    feat(Arrays/H.Longest Valid Parentheses-1.py): first attempt completed 2025-04-06

--- Arrays/H.Longest Valid Parentheses-1.py
+++ Arrays/H.Longest Valid Parentheses-1.py
@@ -1,3 +1,24 @@
 class Solution:
     def longestValidParentheses(self, s: str) -> int:
-        
+        res = 0
+        l, r = 0, 0
+        for c in s:
+            if c == '(':
+                l += 1
+            else:
+                r += 1
+            if l == r:
+                res = max(res, r * 2)
+            elif r > l:
+                l, r = 0, 0
+        l, r = 0, 0
+        for c in reversed(s):
+            if c == '(':
+                l += 1
+            else:
+                r += 1
+            if l == r:
+                res = max(res, r * 2)
+            elif l > r:
+                l, r = 0, 0
+        return res
